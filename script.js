694a19cbed5d45c92f49cf26d1bc07cc4ae38110
Brian Monge <bm0801389031@gmail.com>
2021-07-26 13:28:28 -0700

    added comment

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // 3. begin with a function called computerPlay that will randomly return either ‘Rock’, ‘Paper’ or ‘Scissors’. We’ll use this function in the game to make the computer’s play. Tip: use the console to make sure this is returning the expected output before moving to the next step!
-
+//added new code
 function computerPlay(){
     const compOutputs = ['rock', 'paper', 'scissors'];
     let compRandom = Math.floor(Math.random() * compOutputs.length);
